fd059f6d961a8f972088a0208db8a153539c331c
flo <ufo.1997@qq.com>
2022-11-22 22:39:52 +0800

    优化逻辑

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
 import boto3
 from flask import Flask, request
 
+app = Flask(__name__)
 
-server=Flask(__name__)
 
-ak,sk='',''
-client = boto3.client('translate',region_name=ak, aws_access_key_id=sk)
-
-@server.route('/translate',methods=['post','get'])
+@app.route('/translate', methods=['post', 'get'])
 def translate():
+    try:
+        data = request.args
+        # get aws ak and sk
+        ak, sk = data.get('ak'), data.get('sk')
+        # and source_lanuage and destination_lanuage, source_lanuage = 'en', destination_lanuage = 'zh'
+        source_language, destination_language = data.get('src', 'en'), data.get('dst', 'zh')
+        message = data.get('msg', 'this a test message')
 
-    data = request.args
-    # source_lanuage = 'en'
-    # destination_lanuage = 'zh'
-    source_lanuage = data.get('src')
-    destination_lanuage = data.get('dst')
-    message = data.get('msg') 
+        client = boto3.client('translate', region_name=ak, aws_access_key_id=sk)
 
-    response=client.translate_text(
-        Text=message,
-        SourceLanguageCode=source_lanuage,
-        TargetLanguageCode=destination_lanuage,
-    )
-
+        response = client.translate_text(
+            Text=message,
+            SourceLanguageCode=source_language,
+            TargetLanguageCode=destination_language,
+        )
+    except Exception as e:
+        print(e)
 
     return {
-        "from": f"{source_lanuage}",
-        "to": f"{destination_lanuage}",
+        "from": f"{source_language}",
+        "to": f"{destination_language}",
         "trans_result": [
-        {
-            "src": message,
-            "dst": response['TranslatedText']
-        }
-    ]
-}
+            {
+                "src": message,
+                "dst": response.get('TranslatedText', "no message pls check script")
+            }
+        ]
+    }
 
-server.run(port=8888, host='0.0.0.0',debug=True)
+
+app.run(port=8888, host='0.0.0.0', debug=True)
--- 关于potplayer.md
+++ 关于potplayer.md
@@ -3,12 +3,19 @@
 
 这个插件使用的是AngelScript, 这个语言很小众。基本也就只有官方文档可以参考。
 
-
 http://200t.w3cschool.cn/angelscript/
 
 http://www.angelcode.com/angelscript/documentation.html
 
-
 目前猜测是调用了视频播放器自带的钩子函数。
 
 看到了有人制作了关于百度api翻译实时字幕的教程，然后去github翻了下作者的源代码，以此为灵感，写了一个基于aws的翻译插件，并且用flask 实现一个api的封装，api内部调用了aws的翻译服务。
+
+![img.png](img.png)
+
+
+翻译效果图
+![img_1.png](img_1.png)
+
+
+![img_2.png](img_2.png)

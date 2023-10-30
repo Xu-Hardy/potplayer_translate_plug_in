37613aefc61cff25d7e3303cdd4af5dc3f1cd325
Xu <deep.work.max@gmail.com>
2023-10-30 20:26:33 +0800

    add flask

--- translate_api/translate.py
+++ translate_api/translate.py
@@ -1,65 +1,65 @@
 import logging
 import os
 import boto3
-from flask import Flask, request
-# from dotenv import load_dotenv
-
-# load_dotenv()
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# read ak/sk from env
-ak, sk = os.environ.get('aws_access_key_id'), os.environ.get('aws_secret_access_key')
+# Set default from environment variables
+default_ak = os.environ.get('aws_access_key_id')
+default_sk = os.environ.get('aws_secret_access_key')
 region = os.environ.get('region', 'us-east-1')
-client=client = boto3.client('translate', region_name=region, aws_access_key_id=ak, aws_secret_access_key=sk)
+client = boto3.client('translate', region_name=region, aws_access_key_id=default_ak, aws_secret_access_key=default_sk)
 
-logger = logging.getLogger()
-# boto3.set_stream_logger('', level=logging.DEBUG)
-
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
     return {
-        'heath check': 'pass'
+        'health check': 'pass'
     }
 
-
-@app.route('/translate', methods=['post', 'get'])
+@app.route('/translate', methods=['POST', 'GET'])
 def translate():
-    """
-    require: msg
-    :return:
-    """
-    # try:
-    data = request.args
-    # get aws ak and sk
-    # and source_language and destination_language, for default, source_language = 'en', destination_language = 'zh'
-    source_language, destination_language = data.get('src', 'en'), data.get('dst', 'zh')
-    message = data.get('msg', 'this a test message')
-    ak = data.get('ak', 'this a test message')
-    sk = data.get('sk', 'this a test message')
-    global client
-    if client is None:
-        client = boto3.client('translate', region_name=region, aws_access_key_id=ak, aws_secret_access_key=sk) if ak is not None and sk is not None else None
+    try:
+        data = request.args
 
-    response = client.translate_text(
-        Text=message,
-        SourceLanguageCode=source_language,
-        TargetLanguageCode=destination_language,
-    )
-    print(response)
+        # Use provided ak/sk or fallback to environment variable
+        ak = data.get('ak', default_ak)
+        sk = data.get('sk', default_sk)
 
-    return {
-        "from": f"{source_language}",
-        "to": f"{destination_language}",
-        "trans_result": [
-            {
-                "src": message,
-                "dst": response.get('TranslatedText', "no message pls check script")
-            }
-        ]
-    }
+        # If both are still None, return an error
+        if not ak or not sk:
+            return jsonify(error="Missing AWS credentials"), 400
 
+        source_language = data.get('src', 'en')
+        destination_language = data.get('dst', 'zh')
+        message = data.get('msg', 'this a test message')
+
+        global client
+        client = boto3.client('translate', region_name=region, aws_access_key_id=ak, aws_secret_access_key=sk)
+
+        response = client.translate_text(
+            Text=message,
+            SourceLanguageCode=source_language,
+            TargetLanguageCode=destination_language,
+        )
+
+        logger.info(response)
+        return jsonify(
+            from_language=source_language,
+            to_language=destination_language,
+            trans_result=[
+                {
+                    "src": message,
+                    "dst": response.get('TranslatedText', "No translation available")
+                }
+            ]
+        )
+    except Exception as e:
+        logger.error(f"Error translating text: {e}")
+        return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=10000)
